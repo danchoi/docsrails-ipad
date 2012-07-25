c4181cd2c040c1774612062d6c4d7e7bd35e6177
Daniel Choi <dhchoi@gmail.com>
2012-07-24 22:48:26 -0400

    sqlite3 schema

---
+++ schema.sql
@@ -0,0 +1,14 @@
+
+
+create table pages (
+  item_id text,
+  page_title text,
+  title text,
+  slug text,
+  description text,
+  source text
+);
+
+create unique index pages_page_id_idx on pages (item_id);
+create index pages_title_idx on pages (title);
+
