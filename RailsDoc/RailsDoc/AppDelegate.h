483284bf150d013a6347ca6a2385f760e3f62b97
Daniel Choi <dhchoi@gmail.com>
2012-07-25 10:20:26 -0400

    create ipad project

---
+++ RailsDoc/RailsDoc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RailsDoc
+//
+//  Created by Daniel Choi on 7/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
